2208781b264486574b307a2031ac2e6138869e18
Mariana <mariana.daruix@gmail.com>
2023-02-23 17:59:38 -0300

    Atualiza AppStarWars

---
+++ AppStarWars/src/components/star-wars/star-wars.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-wars',
+  templateUrl: './star-wars.component.html',
+  styleUrls: ['./star-wars.component.css']
+})
+export class StarWarsComponent {
+
+}
